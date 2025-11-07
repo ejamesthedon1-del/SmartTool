646008491c7fb8a425b374ef7d93e14ec5d4b6c2
Eric <ejamesthedon1@gmail.com>
2025-11-07 00:17:40 +0000

    port again

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["8vwc36-2222.csb.app"], // 👈 your sandbox host
+    allowedHosts: ["8vwc36-2223.csb.app"], // 👈 your sandbox host
     host: true,
     port: 2223,
   },
